21845e49caff192efe0f47b183d1a8b9b0110b5c
JustJohn001 <john.ndungu@gmail.com>
2023-07-25 03:01:48 -0700

    group project. John & Sharon

--- _putchar.c
+++ _putchar.c
@@ -7,4 +7,14 @@
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
+#include <unistd.h>
+/**
+ * _putchar - write character c to the stdout
+ * @c: the character
+ * Return: 1 on success and -1 otherwise
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 }
